7c66f13be50a7b5736b6ea014d60403cfa1d75a5
Prayansh17 <agarwa_941942@student.nitw.ac.in>
2022-11-28 03:26:26 +0530

    gcov updated to contain result for all test suites

--- program_spectra.py
+++ program_spectra.py
@@ -4,13 +4,26 @@ import os
 import glob
 import subprocess
 
+
 problem_name = 'Problem1'
 mutant_folder = 'NTS/'+problem_name+'_mutants'
 mutant_folders = glob.glob(mutant_folder+'/*')
 
+testsuite_folder = 'NTS/'+problem_name+'_test_suite'
+testsuite_folders = glob.glob(testsuite_folder+'/*')
+
+
 for i in range(len(mutant_folders)):
+
     os.system('gcc -c -Wall -pg -fprofile-arcs -ftest-coverage NTS/'+problem_name+'_mutants/v'+str(i+1)+'/'+problem_name+'.c')# -o NTS/'+problem_name+'_mutants/v'+str(i+1)+'/a.out')
     os.system('gcc -Wall -pg -fprofile-arcs -ftest-coverage '+problem_name+'.o')
+
+    # executing the program
+    for j in range(len(testsuite_folders)):
+
+        # os.system('./NTS/'+problem_name+'_mutants/v'+str(i+1)+'/a.out 1 2 3 4 5')
+        os.system('xargs -a NTS/'+problem_name+'_test_suite/input.'+str(j+1)+' ./a.out')
+
     # move object file to folder
     os.system('mv '+problem_name+'.o NTS/'+problem_name+'_mutants/v'+str(i+1)+'/')
     # move gcno file to folder
@@ -18,8 +31,8 @@ for i in range(len(mutant_folders)):
     # move a.out file to folder
     os.system('mv a.out NTS/'+problem_name+'_mutants/v'+str(i+1)+'/')
 
-    # executing the program
-    os.system('./NTS/'+problem_name+'_mutants/v'+str(i+1)+'/a.out 1 2 3 4 5')
+    
+
     # move gcda file to folder
     os.system('mv '+problem_name+'.gcda NTS/'+problem_name+'_mutants/v'+str(i+1)+'/')
     # move gmon file to folder
@@ -27,8 +40,8 @@ for i in range(len(mutant_folders)):
     # Creating gcov coverage report
     os.system('gcov NTS/'+problem_name+'_mutants/v'+str(i+1)+'/'+problem_name+'.c')
     os.system('mv '+problem_name+'.c.gcov NTS/'+problem_name+'_mutants/v'+str(i+1)+'/')
-    for j in range(0, 100):
-        os.system('xargs -a NTS/'+problem_name+'_test_suite/input.'+str(j+1)+' NTS/'+problem_name+'_mutants/v'+str(i+1)+'/a.out')#> NTS/'+problem_name+'_mutant_output/v'+str(i+1)+'/output.'+str(j+1))
+    # for j in range(0, 100):
+        # os.system('xargs -a NTS/'+problem_name+'_test_suite/input.'+str(j+1)+' NTS/'+problem_name+'_mutants/v'+str(i+1)+'/a.out')#> NTS/'+problem_name+'_mutant_output/v'+str(i+1)+'/output.'+str(j+1))
         # os.system('gcov NTS/'+problem_name+'_mutants/v'+str(i+1)+'/'+problem_name+'.c'
-        print('done')
+        # print('done')
         # os.system('mv '+problem_name+'.c.gcov NTS/'+problem_name+'_mutant_output/v'+str(i+1)+'/gcov.'+str(j+1))
