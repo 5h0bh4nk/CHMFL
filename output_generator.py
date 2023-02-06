63e16face0ff2a3b55c77495d6faba8e5271bbde
Shubhank <kul.shubhank207@gmail.com>
2023-02-06 17:24:10 +0530

    generic code

--- output_generator.py
+++ output_generator.py
@@ -1,21 +1,31 @@
 # 1. generating oracle outputs
 import os
 
+root_folder = 'NTS_Repo/'
+problem_folder = 'Problem1/'
 problem_name = 'Problem1'
-for i in range(1, 101):
-  os.system('xargs -a NTS/'+problem_name+'_test_suite/input.'+str(i)+' ./a.out > NTS/'+problem_name+'_oracle_output/output.'+str(i))
 
+# get length of test_suite folder
+testsuite_folder = root_folder + problem_folder +  problem_name + '_test_suite/'
+test_suite_files = os.listdir(testsuite_folder)
 
+# run actual program
+
+os.system('gcc -w '+ root_folder + problem_folder + problem_name + '_source/' + problem_name + '.c') # -o '+ root_folder + problem_folder + problem_name + '/a.out')
+for i in range(1, len(test_suite_files)+1):
+  os.system('xargs -a '+ testsuite_folder +'input.'+str(i)+' ./a.out > '+ root_folder + problem_folder + problem_name + '_oracle_output/output.'+str(i))
+  print('Ran test case '+str(i)+' of '+str(len(test_suite_files)))
+
+print('Done generating oracle outputs')
 # check number of folder for mutants
 import glob
 
-problem_name = 'Problem1'
-mutant_folder = 'NTS/'+problem_name+'_mutants' 
-mutant_folders = glob.glob(mutant_folder+'/*')
+mutant_folder = root_folder + problem_folder +  problem_name + '_mutants/' 
+mutant_folders = glob.glob(mutant_folder+'*')
 
 for i in range(len(mutant_folders)):
-  os.system('mkdir NTS/'+problem_name+'_mutant_output/v'+str(i+1))
+  os.system('mkdir '+ root_folder + problem_folder + problem_name+'_mutant_output/v'+str(i+1))
   # compile teh program
-  os.system('gcc -w NTS/'+problem_name+'_mutants/v'+str(i+1)+'/'+problem_name+'.c') # -o NTS/'+problem_name+'_mutants/v'+str(i+1)+'/a.out')
-  for j in range(0, 100):
-    os.system('xargs -a NTS/'+problem_name+'_test_suite/input.'+str(j+1)+' ./a.out > NTS/'+problem_name+'_mutant_output/v'+str(i+1)+'/output.'+str(j+1))
+  os.system('gcc -w '+ root_folder + problem_folder + problem_name+'_mutants/v'+str(i+1)+'/'+problem_name+'.c') # -o '+ root_folder + problem_folder + problem_name+'_mutants/v'+str(i+1)+'/a.out')
+  for j in range(0, len(test_suite_files)):
+    os.system('xargs -a '+ root_folder + problem_folder + problem_name+'_test_suite/input.'+str(j+1)+' ./a.out > '+ root_folder + problem_folder + problem_name+'_mutant_output/v'+str(i+1)+'/output.'+str(j+1))
