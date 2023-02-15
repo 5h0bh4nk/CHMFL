4e0a9acf6cdd2fcd4a75f943d5b03f2a58bacbb9
Shubhank <kul.shubhank207@gmail.com>
2023-02-15 17:11:20 +0530

    final

--- config.py
+++ config.py
@@ -2,7 +2,10 @@
 import os
 import glob
 
+
 root_folder = 'NTS_Repo/'
+
+
 problem_folder = 'cfg_test/'
 problem_name = 'cfg_test'
 mutant_folder = root_folder+problem_folder+problem_name+'_mutants'
@@ -10,3 +13,11 @@ mutant_folders = glob.glob(mutant_folder+'/*')
 
 testsuite_folder = root_folder + problem_folder +  problem_name + '_test_suite/'
 test_suite_files = os.listdir(testsuite_folder)
+
+
+for problems in os.listdir(root_folder):
+    problem_folder = problems+"/"
+    problem_name = problems
+    os.system('python3 gcov_generator.py')
+    os.system('python3 output_generator.py')
+    os.system('python3 helper.py')
