7e97adaa04def8efa54c6eb4f8aeb984c1e0f80a
Shubhank <kul.shubhank207@gmail.com>
2023-02-20 16:03:43 +0530

    p1 v8

--- output_generator.py
+++ output_generator.py
@@ -11,6 +11,8 @@ os.system('mkdir '+ problem_folder +'_oracle_output')
 
 os.system('gcc -w '+ problem_folder  + '_source/' + problem_name + '.c') # -o '+ problem_folder  + '/a.out')
 for i in range(1, len(test_suite_files)+1):
+  if os.path.exists(problem_folder  + '_oracle_output/output.'+str(i)):
+    continue
   os.system('xargs -a '+ testsuite_folder +'input.'+str(i)+' ./a.out > '+ problem_folder  + '_oracle_output/output.'+str(i))
   print('Ran test case '+str(i)+' of '+str(len(test_suite_files)))
 
@@ -26,5 +28,6 @@ for i in range(len(mutant_folders)):
 
     if not os.path.exists('./'+problem_folder +'_mutant_output/v'+str(i+1)+'/output.'+str(j+1)):
       os.system('xargs -a '+ problem_folder +'_test_suite/input.'+str(j+1)+' ./a.out > '+ problem_folder +'_mutant_output/v'+str(i+1)+'/output.'+str(j+1))
+      print('Ran input '+str(j+1)+' Mutant '+str(i+1))
     else:
       print('Already exists !')
