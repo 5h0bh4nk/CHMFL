2de75a9373745544982249b9535221b4785c5b6c
Shubhank <kul.shubhank207@gmail.com>
2023-03-01 14:36:47 +0530

    other added

---
+++ plot_others.py
@@ -0,0 +1,29 @@
+from config import *
+import csv
+import os
+import pandas as ps
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot(filename):
+    df = ps.read_csv(f'./others/{filename}.csv')
+    # df.sort_values(df.columns[1],axis=0,inplace=True)
+
+    # # cummulative value of x
+    # df[df.columns[1]] = df[df.columns[1]].cumsum()
+
+    print(df[df.columns[0]])
+    print(df.head())
+
+
+    # plt.figure(figsize=(10,10))
+    plt.plot(df[df.columns[0]],df[df.columns[1]])
+    plt.ylabel('Version')
+    plt.xlabel('Line Number')
+    plt.title(f'{filename}')
+    plt.savefig(f'./output/{filename}.png')
+    plt.show()
+
+    # print(df)
+
+plot("dstar")
