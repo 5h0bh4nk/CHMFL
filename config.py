26127ea0ba189f838d85e89aa4a167c04e8bef47
shubhank <kul.shubhank207@gmail.com>
2023-04-19 14:49:21 +0530

    chNGES

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ locs ={
 }
 
 root_folder = 'SIMENS_REPOSITORY/'
-problem = 'schedule/'
-problem_name = 'schedule'
+problem = 'tcas/'
+problem_name = 'tcas'
 problem_folder = root_folder+problem+problem_name
 
 mutant_folder = problem_folder +'_mutants'
